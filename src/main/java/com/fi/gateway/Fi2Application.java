b1bdfef8502ff3e4936fd3f8beb81ed11f027e7b
anant3007 <46474294+anant3007@users.noreply.github.com>
2019-02-13 19:14:47 +0530

    committing changes o FI2

---
+++ src/main/java/com/fi/gateway/Fi2Application.java
@@ -0,0 +1,14 @@
+package com.fi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fi2Application.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/fi/gateway/controller/FIGatewayController.java
@@ -0,0 +1,43 @@
+package com.fi.gateway.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fi.gateway.model.AcctKeys;
+import com.fi.gateway.model.DebitAddRs;
+import com.fi.gateway.model.DebitInfo;
+import com.fi.gateway.model.DebitKeys;
+import com.fi.gateway.model.DebitStatus;
+
+@RestController
+@RequestMapping("/api")
+public class FIGatewayController {
+
+	@PutMapping(value="/fi/debit",produces=MediaType.APPLICATION_XML_VALUE,consumes=MediaType.APPLICATION_XML_VALUE)
+	public DebitAddRs debit(@RequestBody DebitInfo debitInfo)
+	{
+		DebitAddRs response= new DebitAddRs();	
+		response.setSeverity("Info");
+		response.setStatusCode(""+0);
+		response.setSvcProviderName("DNA");
+		
+		AcctKeys ak= new AcctKeys();
+		ak.setAcctId(""+560731492);
+		ak.setAcctType("SDA");
+		
+		DebitKeys dk = new DebitKeys();
+		dk.setDebitId("123");
+		dk.setAcctKeys(ak);
+		
+		DebitStatus ds = new DebitStatus();
+		ds.setDebitStatusCode("Posted from FI 2");
+		ds.setEffDt("2018-18-01T10:12:12.211");
+		response.setDebitKeys(dk);
+		response.setDebitStatus(ds);
+		
+		return response;		
+	}
+}
